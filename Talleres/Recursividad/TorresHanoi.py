c12439d72fe22151b89e7b79257c4dfa7d162d3d
dahernando <69641310+dahernando@users.noreply.github.com>
2020-08-15 13:03:36 -0500

    Add files via upload

--- Talleres/Recursividad/TorresHanoi.py
+++ Talleres/Recursividad/TorresHanoi.py
@@ -1,4 +1,4 @@
-i = int(input("Ingrese un numero"))
+i = int(input("Ingrese el numero de platos"))
 def tor(n):
     if n==0:
         return 1
@@ -6,5 +6,5 @@ def tor(n):
         x=(2*tor(n-1))-1
         return x
     else:
-        return 2*tor(n-1)
+        return (2*tor(n-1))-(1/i-1)
 print(tor(i))
