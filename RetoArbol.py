62ecb551469b986b6f71d5919e1b001b46b49dca
dahernando <69641310+dahernando@users.noreply.github.com>
2020-10-18 21:10:20 -0500

    Add files via upload

---
+++ RetoArbol.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 18 17:38:32 2020
+
+@author: daho
+"""
+
+import os
+
+class node():
+    def __init__(self, dato):
+        self.left = None
+        self.right = None
+        self.dato = dato
+
+class arbol():
+    def __init__(self):
+        self.root = None
+        
+    def insert(self, a, dato):
+        if self.search(dato,a)!=None:
+             return a
+        if a == None:
+            a = node(dato)
+        else:
+            d = a.dato
+            if dato < d:
+                a.left = self.insert(a.left, dato)
+            else:
+                a.right = self.insert(a.right, dato)
+        return a
+
+    def inorder(self, a):
+        if a == None:
+            return None
+        else:
+            self.inorder(a.left)
+            print(a.dato)
+            self.inorder(a.right)
+
+    def preorder(self, a):
+        if a == None:
+            return None
+        else:
+            print(a.dato)
+            self.preorder(a.left)
+            self.preorder(a.right)
+
+    def postorder(self, a):
+        if a == None:
+            return None
+        else:
+            self.postorder(a.left)
+            self.postorder(a.right)
+            print(a.dato)
+
+    def search(self, dato, a):
+        if a == None:
+            return None
+        else:
+            if dato == a.dato:
+                return a.dato
+            else:
+                if dato < a.dato:
+                    return self.search(dato, a.left)
+                else:
+                    return self.search(dato, a.right)
+    
+    
+    #######################################################
+    
+    def minValueNode(self,node): 
+        self.current = node 
+  
+        while(self.current.left is not None): 
+            self.current = self.current.left  
+  
+        return self.current
+    
+    def deleteNode(self,a, value): 
+        if a.dato is None: 
+            return a  
+        
+        if value < a.dato: 
+            a.left = self.deleteNode(a.left, value) 
+  
+        elif(value > a.dato): 
+            a.right = self.deleteNode(a.right, value) 
+  
+        else: 
+            if a.left is None : 
+                temp = a.right  
+                a = None 
+                return temp  
+              
+            elif a.right is None: 
+                temp = a.left  
+                a = None
+                return temp 
+                
+            temp = self.minValueNode(a.right) 
+  
+            a.dato = temp.dato 
+  
+            a.right = self.deleteNode(a.right,temp.dato) 
+  
+  
+        return a  
+
+tree = arbol()
+
+while True:
+    os.system("cls")
+    print("Arbol ABB")
+    opc = input("\n1.-Insertar nodo \n2.-Inorden \n3.-Preorden \n4.-Postorden \n5.-Buscar \n6.-Eliminar dato \n7.-salir \n\nElige una opcion -> ")
+
+    if opc == '1':
+        nodo = input("\nIngresa el nodo -> ")
+        if nodo.isdigit():
+            nodo = int(nodo)
+            tree.root = tree.insert(tree.root, nodo)
+        else:
+            print("\nIngresa solo digitos...")
+    elif opc == '2':
+        if tree.root == None:
+            print("Vacio")
+        else:
+            tree.inorder(tree.root)
+    elif opc == '3':
+        if tree.root == None:
+            print("Vacio")
+        else:
+            tree.preorder(tree.root)
+    elif opc == '4':
+        if tree.root == None:
+            print("Vacio")
+        else:
+            tree.postorder(tree.root)
+    elif opc == '5':
+        nodo = input("\nIngresa el nodo a buscar -> ")
+        if nodo.isdigit():
+            nodo = int(nodo)
+            if tree.buscar(nodo, tree.root) == None:
+                print("\nNodo no encontrado...")
+            else:
+                print("\nNodo encontrado -> ",tree.buscar(nodo, tree.root), " si existe...")
+        else:
+            print("\nIngresa solo digitos...")        
+    elif opc == '6':
+        number= int(input("Ingrese el numero a elminar"))
+        tree.deleteNode(tree.root,number)
+    elif opc == '7':
+        print("\nElegiste salir...\n")
+        os.system("pause")
+        break
+    else:
+        print("\nElige una opcion correcta...")
+    print()
+    os.system("pause")
+
+print()
